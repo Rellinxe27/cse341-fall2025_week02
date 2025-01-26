f23f357999e4a506d0d878523d77849e50b4a30b
Rellinxe27 <bon19013@byui.edu>
2025-01-26 01:01:09 +0000

    first commit (first assignment)

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const someoneNameRoutes = require('./someoneName');
 // Use routes
 router.use('/name', someoneNameRoutes); // This will prefix all name routes with /name
 
-// Base route test (if you want to keep it)
+// Base route test
 router.get('/', async (req, res) => {
     try {
         res.send('Skyla Alliana');
@@ -16,42 +16,5 @@ router.get('/', async (req, res) => {
         res.status(500).json({ message: error.message });
     }
 });
-/*
-// GET
-router.get('/:id', async (req, res) => {
-    try {
-        res.send(`Get contact ${req.params.id}`);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-
-// POST new contact
-router.post('/', async (req, res) => {
-    try {
-        res.status(201).send('Create new contact');
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-// PUT update contact
-router.put('/:id', async (req, res) => {
-    try {
-        res.send(`Update contact ${req.params.id}`);
-    } catch (error) {
-        res.status(400).json({ message: error.message });
-    }
-});
-
-// DELETE contact
-router.delete('/:id', async (req, res) => {
-    try {
-        res.send(`Delete contact ${req.params.id}`);
-    } catch (error) {
-        res.status(500).json({ message: error.message });
-    }
-});
-*/
 
 module.exports = router;
