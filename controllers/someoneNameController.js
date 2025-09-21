b6e93ae35826f1814c4fa89e91d2b8b4e8ba35de
Rellinxe27 <bon19013@byui.edu>
2025-09-21 00:39:42 +0000

    commit

--- controllers/someoneNameController.js
+++ controllers/someoneNameController.js
@@ -1,6 +1,6 @@
 const getName = async (req, res) => {
     try {
-        res.send('Get someonee name');
+        res.send('Get someone name');
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
